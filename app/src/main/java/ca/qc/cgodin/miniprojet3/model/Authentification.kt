8cc3dc3135994e59e457b553fd0f3c5cfb4475f8
Andrei Cretu2 <andrei.cretu4676@gmail.com>
2025-11-03 16:18:42 -0500

    showing user name once logged in

--- app/src/main/java/ca/qc/cgodin/miniprojet3/model/Authentification.kt
+++ app/src/main/java/ca/qc/cgodin/miniprojet3/model/Authentification.kt
@@ -1,8 +1,14 @@
 package ca.qc.cgodin.miniprojet3.model
 
 object AuthUtils {
+
+    var utilisateur: String? = null
     fun makeAuthKey(identifiant: String, motDePasse: String): String {
         val digits = motDePasse.takeLast(5)
         return identifiant + digits
+    }
+
+    fun getUser(prenom: String, nom: String){
+        utilisateur = "$prenom $nom"
     }
 }
