8484782cd4b7ec515a890736cbca2f5f32bd62ff
Andrei Cretu2 <andrei.cretu4676@gmail.com>
2025-11-04 17:30:07 -0500

    missing toast and lifecyclescope imports

--- app/src/main/java/ca/qc/cgodin/miniprojet3/viewModel/ListeSucursales.kt
+++ app/src/main/java/ca/qc/cgodin/miniprojet3/viewModel/ListeSucursales.kt
@@ -5,9 +5,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
